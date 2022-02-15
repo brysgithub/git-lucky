34a664c82625a5e72c0b6ddc7e557721b5ce8742
Jonathan Chen <jontkc98@gmail.com>
2022-02-14 21:04:40 -0800

    Updated schema.drawio and changed the model files for User, Game, Transaction, and index accordingly

---
+++ models/Transaction.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Transaction extends Model {}
+
+Transaction.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+          model: 'user',
+          key: 'id',
+          unique: true
+      }
+    },
+    game_id: {
+      type: DataTypes.INTEGER,
+      references: {
+          model: 'game',
+          key: 'id',
+          unique: false
+      }
+    },
+    result: {
+      // TODO: make sure this works or change it to INTEGER and 1's and 0's
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    }
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'transaction',
+  }
+);
+
+module.exports = Transaction;
