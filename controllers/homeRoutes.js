57778a086a136fe99e1f50a989edb7d75147ffd3
Jonathan Chen <jontkc98@uw.edu>
2022-02-23 19:51:31 -0800

    Got login routes to work

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,9 +2,8 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
   try {
-
     // Pass serialized data and session flag into template
     res.render('homepage', {
       logged_in: req.session.logged_in 
@@ -38,7 +37,7 @@ router.get('/profile', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/homepage');
     return;
   }
 
