2214743ea2539822862e0c3402ae02f02af2dbdb
Jonathan Chen <jontkc98@uw.edu>
2022-02-23 20:21:16 -0800

    stuff with login logout routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,6 +14,18 @@ router.get('/', withAuth, async (req, res) => {
   }
 });
 
+router.get('/homepage', withAuth, async (req, res) => {
+  try {
+    // Pass serialized data and session flag into template
+    res.render('homepage', {
+      logged_in: req.session.logged_in 
+    });
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
   try {
